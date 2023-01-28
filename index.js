dbde31a9d699cb022024de5758055586dba51522
amaanlakdawala <107401600+amaanlakdawala@users.noreply.github.com>
2023-01-28 17:40:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+ document.getElementById("java").addEventListener("click", myFunction );
+ function  myFunction(){
+      confirm("Do you Want to confirm your order");
+
+};
+document.getElementById("form_submit").addEventListener("click",
+function submit(){
+  confirm("do you want to confirm your reservation")
+});
+ let n = document.getElementById("name").value
